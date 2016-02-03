34afeb00ba1487324eab970e61f0d342105f472c
dhani1803 <dhani.s.wibowo@gmail.com>
2016-02-03 11:33:04 +0700

    Base Project Student
    Step 1 show data from cache

---
+++ StudentsAPI/Models/IStudentsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsAPI.Models
+{
+    interface IStudentsRepository
+    {
+        IEnumerable<Students> GetAll();
+        Students Get(int Id);
+        Students Add(Students stut);
+        void Remove(int Id);
+        bool Update(Students stut);
+    }
+}
---
+++ StudentsAPI/Models/Students.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentsAPI.Models
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Alamat { get; set; }
+        public int Umur { get; set; }
+    }
+}
